618d0e063af1795015e03a4b17f99a81688ad289
ShivamKumarJha <jha.shivam3@gmail.com>
2019-11-06 12:29:19 +0000

    9 PPBS29.73-81-2 c4cd2 release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

---
+++ lineage_surfna.mk
@@ -0,0 +1,23 @@
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from surfna device
+$(call inherit-product, $(LOCAL_PATH)/device.mk)
+
+PRODUCT_BRAND := motorola
+PRODUCT_DEVICE := surfna
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_NAME := lineage_surfna
+PRODUCT_MODEL := moto e6
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+TARGET_VENDOR := motorola
+TARGET_VENDOR_PRODUCT_NAME := surfna
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC=" 9 PPBS29.73-81-2 c4cd2 release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := 9/PPBS29.73-81-2/c4cd2:user/release-keys
